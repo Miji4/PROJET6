9df1a2b14878da73aedbe3be3e177167235e4bf2
Mhijiba <b.mhijiba@hotmail.fr>
2021-05-25 13:32:43 +0100

    cle secret dans fichier env

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,12 +1,12 @@
 //variable module npm token
 const jwt = require('jsonwebtoken');
-
+require('dotenv').config() // independance qui charge les variable d'environnement
 
 // création du middleware qui va protèger les routes selectionnées et vérifier que l'utilisateur est authentifié avant d'autoriser l envoi de ses requetes
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1]; // selection du token du header 'Authorization' de la requete entrante
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // fonction verify pour décoder le token 
+        const token = req.headers.authorization.split(' ')[1]; // extrait du token du header 'Authorization' de la requete entrante
+        const decodedToken = jwt.verify(token, process.env.CLE_SECRETE); // fonction verify pour décoder le token 
         const userId = decodedToken.userId; // j'extrais l'ID utilisateur du token 
         if(req.body.userId && req.body.userId !== userId) { // comparaison des 2 ID utilisateur (l'id utilisateur et celui extrait du token)
             throw 'Invalid user ID'; // erreur si différent 
